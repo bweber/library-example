b3cb0ce1cfa09be8e1d0fb79761f0beeac69a82f
Brandon Weber <brandon@basistheory.com>
2021-02-28 10:59:59 -0600

    Filter correlationid logs and have swagger handle reverse proxy

--- Library.API/Configuration/SwaggerConfiguration.cs
+++ Library.API/Configuration/SwaggerConfiguration.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Library.Common.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,22 @@ namespace Library.API.Configuration
         {
             if (environment.IsProduction()) return app;
             
-            app.UseSwagger();
+            app.UseSwagger(options =>
+            {
+                options.PreSerializeFilters.Add((swaggerDoc, httpRequest) =>
+                {
+                    if (!httpRequest.Headers.ContainsKey("X-Forwarded-Host")) return;
+
+                    var serverUrl = $"{httpRequest.Headers["X-Forwarded-Proto"]}://" +
+                                    $"{httpRequest.Headers["X-Forwarded-Host"]}/" +
+                                    $"{httpRequest.Headers["X-Forwarded-Prefix"]}";
+
+                    swaggerDoc.Servers = new List<OpenApiServer>
+                    {
+                        new() { Url = serverUrl }
+                    };
+                });
+            });
             app.UseSwaggerUI(c => c.SwaggerEndpoint("v1/swagger.json", "Library API v1"));
 
             return app;
