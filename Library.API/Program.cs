80a992600bb1002c4d9ef3b090f56bb6465d28f7
Brandon Weber <brandon@basistheory.com>
2021-02-28 09:13:06 -0600

    Configure azure keyvault as a config provider

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -1,4 +1,10 @@
+using System;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Library.Common.Extensions;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Library.API
@@ -12,6 +18,17 @@ namespace Library.API
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    if (context.HostingEnvironment.IsAcceptance()) return;
+                
+                    var builtConfig = config.Build();
+                    var secretClient = new SecretClient(
+                        new Uri($"https://{builtConfig.GetValue<string>("KeyVaultName")}.vault.azure.net/"),
+                        new DefaultAzureCredential());
+                    
+                    config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
