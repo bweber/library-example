07bad3c671c8470356c8ed86210c2120d8643e0b
Brandon Weber <brandon@basistheory.com>
2021-09-29 12:42:47 -0500

    Fix test

--- Library.Infrastructure.Tests/Tests/AppServiceTests.cs
+++ Library.Infrastructure.Tests/Tests/AppServiceTests.cs
@@ -15,7 +15,7 @@ namespace Library.Infrastructure.Tests.Tests
         {
             _stackHelper = new MockStackHelper();
         }
-        
+
         [Fact]
         public async Task ShouldSetResourceGroupName()
         {
@@ -28,7 +28,7 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.NotNull(resourceGroupName);
             Assert.Equal("library-rg", resourceGroupName);
         }
-        
+
         [Fact]
         public async Task ShouldSetLocationFromResourceGroup()
         {
@@ -41,17 +41,17 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.NotNull(location);
             Assert.Equal("CentralUS", location);
         }
-        
+
         [Fact]
         public async Task ShouldSetAppServicePlanId()
         {
             var resources = await _stackHelper.TestAsync();
-            
+
             var plan = resources.OfType<Plan>().FirstOrDefault();
             Assert.NotNull(plan);
-            
+
             var planId = await plan.Id.GetValueAsync();
-            
+
             var appService = resources.OfType<AppService>().FirstOrDefault();
             Assert.NotNull(appService);
 
@@ -59,7 +59,7 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.NotNull(appServicePlanId);
             Assert.Equal(planId, appServicePlanId);
         }
-        
+
         [Fact]
         public async Task ShouldBeHttpsOnly()
         {
@@ -72,7 +72,7 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.NotNull(httpsOnly);
             Assert.True(httpsOnly);
         }
-        
+
         [Fact]
         public async Task ShouldUseSystemAssignedIdentity()
         {
@@ -85,7 +85,7 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.NotNull(identity);
             Assert.Equal("SystemAssigned", identity.Type);
         }
-        
+
         [Fact]
         public async Task ShouldHaveAppSettingsSet()
         {
@@ -95,12 +95,12 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.NotNull(keyVault);
 
             var keyVaultName = await keyVault.Name.GetValueAsync();
-            
+
             var appInsights = resources.OfType<Insights>().FirstOrDefault();
             Assert.NotNull(appInsights);
 
             var instrumentationKey = await appInsights.InstrumentationKey.GetValueAsync();
-            
+
             var appService = resources.OfType<AppService>().FirstOrDefault();
             Assert.NotNull(appService);
 
@@ -111,14 +111,14 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.Equal("1800", appSettings["WEBSITES_CONTAINER_START_TIME_LIMIT"]);
             Assert.Equal(_stackHelper.Config.AspnetEnvironment, appSettings["ASPNETCORE_ENVIRONMENT"]);
             Assert.Equal(instrumentationKey, appSettings["APPINSIGHTS_INSTRUMENTATIONKEY"]);
-            Assert.Equal(keyVaultName, appSettings["KeyVaultName"]);
+            Assert.Equal($"https://{keyVaultName}.vault.azure.net/", appSettings["KeyVaultUri"]);
         }
-        
+
         [Fact]
         public async Task ShouldHaveSiteConfigSet()
         {
             var resources = await _stackHelper.TestAsync();
-            
+
             var appService = resources.OfType<AppService>().FirstOrDefault();
             Assert.NotNull(appService);
 
@@ -130,7 +130,7 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.False(siteConfig.RemoteDebuggingEnabled);
             Assert.Equal("1.2", siteConfig.MinTlsVersion);
         }
-        
+
         [Fact]
         public async Task ShouldSetEnvironmentTag()
         {
@@ -144,7 +144,7 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.True(tags.ContainsKey("environment"));
             Assert.Equal("dev", tags["environment"]);
         }
-        
+
         [Fact]
         public async Task ShouldHaveDefaultServiceLogs()
         {
@@ -160,7 +160,7 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.Equal(14, logs.HttpLogs.FileSystem.RetentionInDays);
             Assert.Equal(35, logs.HttpLogs.FileSystem.RetentionInMb);
         }
-        
+
         [Fact]
         public async Task ShouldHaveAnAppPolicyToAccessKeyVault()
         {
@@ -170,27 +170,27 @@ namespace Library.Infrastructure.Tests.Tests
             Assert.NotNull(keyVault);
 
             var vaultId = await keyVault.Id.GetValueAsync();
-            
+
             var appService = resources.OfType<AppService>().FirstOrDefault();
             Assert.NotNull(appService);
 
             var appServiceIdentity = await appService.Identity.GetValueAsync();
-            
+
             var appAccessPolicy = resources.OfType<AccessPolicy>().FirstOrDefault();
             Assert.NotNull(appAccessPolicy);
 
             var keyVaultId = await appAccessPolicy.KeyVaultId.GetValueAsync();
             Assert.NotNull(keyVaultId);
             Assert.Equal(vaultId, keyVaultId);
-            
+
             var tenantId = await appAccessPolicy.TenantId.GetValueAsync();
             Assert.NotNull(tenantId);
             Assert.Equal(_stackHelper.ClientConfig["tenantId"], tenantId);
-            
+
             var principalId = await appAccessPolicy.ObjectId.GetValueAsync();
             Assert.NotNull(principalId);
             Assert.Equal(appServiceIdentity.PrincipalId, principalId);
-            
+
             var secretPermissions = await appAccessPolicy.SecretPermissions.GetValueAsync();
             Assert.Equal(2, secretPermissions.Length);
             Assert.Contains("get", secretPermissions);
