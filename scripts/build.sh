6a8ae17c6551cf0f0c7a45e7c8de05bf25b32629
Brandon Weber <brandon@basistheory.com>
2021-12-12 13:14:40 -0600

    Fix workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ dotnet restore
 
 git rev-parse HEAD > Library.API/version.txt
 
-dotnet build Library.API.sln --no-restore -c Release
+dotnet build Library.sln --no-restore -c Release
 dotnet publish Library.API/Library.API.csproj -c Release -o publish --no-restore --no-build
 
 result=$?
